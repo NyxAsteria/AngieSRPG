d74aaae50d7e4033dccd7477191ba2e93a4651fa
NyxAsteria <thepuzzleexpert@gmail.com>
2021-09-12 01:19:43 -0500

    Bnnuy
    -Added a Drunken Rabbit to the Enemy House, as well as the Critical Claw to the weapon list
    -Added a new map, set at night! This will be the first real battle!
    -Added Nosferatu Drain and the Arcanist class, hopefully this goes alright *rolls eyes*
    -Imported Goinza's HPCost script, now Yaga Carry costs 5 HP
    -Added Noitan Cultist (Arcanist) and an unnamed Cleric to the Enemy House

---
+++ 1.0 Adaptation/Plugin/hpcost-control.js
@@ -0,0 +1,41 @@
+// Plugin by Goinza
+
+var HPCostControl = {
+
+    hasCost: function (item) {
+        var lifeCost = 0;
+        if (item && item.custom.lifeCost) {
+            if (typeof item.custom.lifeCost !== 'number') {
+                throwError029(item);
+            }
+            lifeCost = item.custom.lifeCost;
+        }
+
+        return lifeCost;
+    },
+
+    // Returns a number that represents the max amount of times the unit can use the item with the unit's current HP
+    // Assumes that the check for the lifeCost parameter has been done
+    getTimesPayCost: function (unit, item) {
+        var lifeCost = item.custom.lifeCost;
+        var maxUseCount = Math.floor(unit.getHp() / lifeCost);
+
+        return maxUseCount;
+    },
+
+    // Assumes that the check for the lifeCost parameter has been done
+    canPayCost: function (unit, item) {
+        return this.getTimesPayCost(unit, item) >= 1;
+    },
+
+    // Deals damage to the unit according to the amount of cost it takes from the item
+    // Assumes that the unit's current HP is greater than the damage done by the item
+    payLife: function (unit, item) {
+        if (this.hasCost(item)) {
+            var currentHP = unit.getHp();
+            var lifeCost = item.custom.lifeCost;
+            unit.setHp(currentHp - lifeCost);
+        }
+    }
+
+}
